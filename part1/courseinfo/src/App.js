89db114e3d5fc03b0a0438db9701fac9a48b3e4a
sujon <s.c.shil94@gmail.com>
2023-01-25 23:47:09 +0600

    Part 1: Worked on Courseinfo.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import * as PropTypes from "prop-types";
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const Header = (prop)=>{
+    return <h1>{prop.course}</h1>;
+};
+const Part = (prop) => {
+    return (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            {prop.part} {prop.exercise}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    )
+}
+const Content = (prop)=>{
+
+        return (
+            <div>
+                <Part part={prop.parts[0]['name']} exercise={prop.parts[0]['exercises']}/>
+                <Part part={prop.parts[1]['name']} exercise={prop.parts[1]['exercises']}/>
+                <Part part={prop.parts[2]['name']} exercise={prop.parts[2]['exercises']}/>
+            </div>
+        )
+};
+
+const Total = (prop) => {
+  return (
+      <div>
+          <p>Number of exercises {prop.parts[0]['exercises'] + prop.parts[1]['exercises'] + prop.parts[2]['exercises']}</p>
+      </div>
+  )
+};
+
+const App = () => {
+    const course = "Half Stack application development"
+
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
+
+    return (
+    <div>
+        <Header course={course} />
+        <Content parts = {parts} />
+        <Total parts = {parts} />
     </div>
   );
 }
