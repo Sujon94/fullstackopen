828752eba85cab46077e9d0ee44845927d90692d
sujon <s.c.shil94@gmail.com>
2024-10-12 20:06:43 +0600

    Part2: Exercise 2.1.1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,58 +1,70 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios";
 import Person from "../component/Person";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Sujon Chondro Shil', id: 5, number: '01833801211'},
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ])
-    const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [show, setShow] = useState('');
+    const baseUrl = 'http://localhost:3002/persons';
+
+    useEffect(() => {
+        axios.get(baseUrl)
+            .then(res => {
+                setPersons(res.data)
+            })
+    }, []);
+    /*
+    * Functions:
+    */
     const handleNewName = (e) => {
         setNewName(e.target.value);
     }
 
-    const [newNumber, setNewNumber] = useState('')
     const handleNewNumber = (e) => {
         setNewNumber(e.target.value);
     }
-
+    /**
+     * Let's talk with server
+     * */
+        // Action: Store
     const storeContact = (e) => {
-        e.preventDefault();
-        let err = 0;
-        persons.map((item, index) => {
-            if (item.name.toUpperCase() === newName.toUpperCase()) {
-                err = 1;
-            }
-        })
+            e.preventDefault();
+            let err = 0;
+            persons.map((item, index) => {
+                if (item.name.toUpperCase() === newName.toUpperCase()) {
+                    err = 1;
+                }
+            })
 
-        if (err === 0) {
-            const contactObj = {
-                name: newName,
-                number: newNumber,
-                id: String(persons.length + 1)
-            }
+            if (err === 0) {
+                const contactObj = {
+                    name: newName,
+                    number: newNumber
+                };
+                axios.post(baseUrl, contactObj)
+                    .then(res => {
+                        setPersons(persons.concat(res.data));
+                        setNewName('');
+                        setNewNumber('');
+                    });
 
-            setPersons(persons.concat(contactObj));
-            setNewName('');
-            setNewNumber('')
-        } else {
-            alert(newName + ' is already added to phonebook.')
+            } else {
+                alert(newName + ' is already added to phonebook.')
+            }
         }
-    }
 
-
-    const [show, setShow] = useState('');
-
+    // Action: Get
     const contactList = (show !== '') ? (persons.filter(
         (per) => {
             if (per.name.toLowerCase().includes(show.toLowerCase()) || per.number.includes(show)) {
                 return per;
             }
         })) : persons;
-
+    /**
+     * Visualization
+     * */
     return (
         <div>
             <h2>Phonebook</h2>
@@ -72,7 +84,7 @@ const App = () => {
             </form>
 
             <h3>Numbers</h3>
-            <Person contact={contactList} />
+            <Person contact={contactList}/>
 
         </div>
     )
